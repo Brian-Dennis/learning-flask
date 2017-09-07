d5a05b9d237ca57e3097b11deb4bb9ddd86a27b3
Brian Dennis <briandennis49@outlook.com>
2017-09-06 18:41:39 -0700

    adding in wikimedia geocoder api

---
+++ models.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
+
+import geocoder
+import urllib2
+import json
+
+db = SQLAlchemy()
+
+
+# User Model for database table
+class User(db.Model):
+    __tablename__ = 'users'
+    uid = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    email = db.Column(db.String(150), unique=True)
+    pwdhash = db.Column(db.String(54))
+
+
+    # Constructor for database
+    def __init__(self, firstname, lastname, email, password):
+        self.firstname = firstname.title()
+        self.lastname = lastname.title()
+        self.email = email.lower()
+        self.set_password(password)
+
+        # hashing password
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
+
+        # checking password against stored hash password
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+
+# p = Place()
+# places = p.query("1600 Amphitheater Parkway Mountain View CA")
+class Places(object):
+  def meters_to_walking_time(self, meters):
+    # 80 meters is one minute walking time
+    return int(meters / 80)
+
+  def wiki_path(self, slug):
+    return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+
+  def address_to_latlng(self, address):
+    g = geocoder.google(address)
+    return (g.lat, g.lng)
+
+  def query(self, address):
+    lat, lng = self.address_to_latlng(address)
+    print lat, lng
+
+    query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
+    g = urllib2.urlopen(query_url)
+    results = g.read()
+    g.close()
+
+    data = json.loads(results)
+    print data
+
+    places = []
+    for place in data['query']['geosearch']:
+      name = place['title']
+      meters = place['dist']
+      lat = place['lat']
+      lng = lng['lon']
+
+      wiki_url = self.wiki_path(name)
+      walking_time = self.meters_to_walking_time(meters)
+
+      d = {
+        'name': name,
+        'url': wiki_url,
+        'time': walking_time,
+        'lat': lat,
+        'lng': lng
+      }
+
+      places.append(d)
+
+    return places
--- routes.py
+++ routes.py
@@ -1,14 +1,127 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User, Places
+from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
 
-@app.route("/")
+
+# configuring Database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/learning-flask'
+db.init_app(app)
+
+
+# CSRF secret key for development
+app.secret_key = 'development-key'
+
+
+# Index route
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+
+# About route
+@app.route('/about')
 def about():
     return render_template("about.html")
 
-if __name__ == "__main__":
+
+# Signup route
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  if 'email' in session:
+    return redirect(url_for('home'))
+
+    form = SignupForm()
+
+    # Validating form
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+          # Getting form data ready for submission
+          newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+
+          # Adding and commiting form data to db with newuser variable from above.
+          db.session.add(newuser)
+          db.session.commit()
+
+          # Creating a user session redirect home
+          session['email'] = newuser.email
+          return redirect(url_for('home'))
+
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
+
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if 'email' in session:
+    return redirect(url_for('home'))
+  form = LoginForm()
+
+  # Validating Form
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('login.html', form=form)
+    else:
+      # email and password are a match
+      email = form.email.data
+      password = form.password.data
+
+      # Getting the user by specified email. from db, email unique key
+      user = User.query.filter_by(email=email).first()
+      if user is not None and user.check_password(password):
+
+        session['email'] = form.email.data
+        return redirect(url_for('home'))
+      else:
+        return redirect(url_for('login'))
+
+  elif request.method == 'GET':
+      return render_template('login.html', form=form)
+
+
+# Logout route
+@app.route('/logout')
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('index'))
+
+
+# Home route
+@app.route('/home')
+def home():
+  if 'email' not in session:
+    return redirect(url_for('login'))
+
+  # Address Form
+  form = AddressForm()
+
+  places = []
+  my_coordinates = (47.5729, 117.6822)
+
+  # Checking form submission
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('home.html', form=form)
+    else:
+      # TODO: get the address
+      address = form.address.data
+
+      # TODO: query for places around it
+      p = Places()
+      my_coordinates = p.address_to_latlng(address)
+      places = p.query(address)
+
+      # TODO: return those results
+      return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+  elif request.method == 'GET':
+    return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+if __name__ == '__main__':
     app.run(debug=True)
